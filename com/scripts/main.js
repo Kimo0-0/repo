9527a3e18ca22dba340ddc5a5a1ba93789ca8f36
Kimo0-0 <115895982+Kimo0-0@users.noreply.github.com>
2023-11-04 22:39:30 +0200

    Add files via upload

--- com/scripts/main.js
+++ com/scripts/main.js
@@ -26,3 +26,18 @@ function resetmode() {
     document.querySelector("nav").classList.add("navbar-light");
   }
 }
+var swiper = new Swiper(".swiper-con", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
